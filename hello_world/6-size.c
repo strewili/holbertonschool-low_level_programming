0781a4651f21436303af1b59aac0fefeb9ae001c
strewili <shouqaaa123@gmail.com>
2026-02-24 21:15:58 +0300

    Update 6-size.c

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -1,19 +1,22 @@
 /*
- * File:6-size.c
+ * File: 6-size.c
  * Auth: strewixi
  */
+
 #include <stdio.h>
+
 /**
- * main - prints the size of various types
- * Return: 0
+ * main - Prints "with proper grammar, but the outcome is
+ *                a piece of art," folowed be any line you like go for it
+ *
+ * Return: Always 0.
  */
 int main(void)
 {
-    printf("Size of a char: %lu byte(s)\n", sizeof(char));
+  printf("Size of a char: %lu byte(s)\n", sizeof(char));
     printf("Size of an int: %lu byte(s)\n", sizeof(int));
     printf("Size of a long int: %lu byte(s)\n", sizeof(long int));
     printf("Size of a long long int: %lu byte(s)\n", sizeof(long long int));
     printf("Size of a float: %lu byte(s)\n", sizeof(float));
-
-    return 0;
+	return (0);
 }
