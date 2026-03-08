727ad18e813364088d8aea59aa183c2d6fdf9952
strewili <shouqaaa123@gmail.com>
2026-03-08 19:30:20 +0000

    s

---
+++ pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * _strspn - gets length of prefix substring
+ * @s: string
+ * @accept: accepted characters
+ *
+ * Return: number of bytes
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+ unsigned int i, j;
+
+ for (i = 0; s[i]; i++)
+ {
+  for (j = 0; accept[j]; j++)
+   if (s[i] == accept[j])
+    break;
+
+  if (!accept[j])
+   break;
+ }
+
+ return (i);
+}
---
+++ pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * _strpbrk - searches a string for any of a set of bytes
+ * @s: string
+ * @accept: characters to match
+ *
+ * Return: pointer to the matching character or NULL
+ */
+char *_strpbrk(char *s, char *accept)
+{
+ int i, j;
+
+ for (i = 0; s[i]; i++)
+ {
+  for (j = 0; accept[j]; j++)
+  {
+   if (s[i] == accept[j])
+    return (s + i);
+  }
+ }
+
+ return (0);
+}
