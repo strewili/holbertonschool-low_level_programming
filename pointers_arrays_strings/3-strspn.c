12bdca2885aacd34372889a37c9764eeae5272c1
strewili <shouqaaa123@gmail.com>
2026-03-09 21:05:52 +0300

    Update 3-strspn.c

--- pointers_arrays_strings/3-strspn.c
+++ pointers_arrays_strings/3-strspn.c
@@ -1,25 +1,27 @@
 #include "main.h"
 
 /**
- * _strspn - gets length of prefix substring
- * @s: string
- * @accept: accepted characters
+ * _strspn - gets the length of a prefix substring
+ * @s: string to check
+ * @accept: accepted bytes
  *
- * Return: number of bytes
+ * Return: number of bytes in the initial segment of s
  */
 unsigned int _strspn(char *s, char *accept)
 {
- unsigned int i, j;
+	unsigned int i, j;
 
- for (i = 0; s[i]; i++)
- {
-  for (j = 0; accept[j]; j++)
-   if (s[i] == accept[j])
-    break;
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; accept[j] != '\0'; j++)
+		{
+			if (s[i] == accept[j])
+				break;
+		}
 
-  if (!accept[j])
-   break;
- }
+		if (accept[j] == '\0')
+			break;
+	}
 
- return (i);
+	return (i);
 }
