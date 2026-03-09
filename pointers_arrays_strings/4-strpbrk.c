2cfc9cd87a2493f5c79ffa48447ec12416c387b2
strewili <shouqaaa123@gmail.com>
2026-03-09 21:06:20 +0300

    Update 4-strpbrk.c

--- pointers_arrays_strings/4-strpbrk.c
+++ pointers_arrays_strings/4-strpbrk.c
@@ -2,23 +2,25 @@
 
 /**
  * _strpbrk - searches a string for any of a set of bytes
- * @s: string
- * @accept: characters to match
+ * @s: string to search
+ * @accept: bytes to match
  *
- * Return: pointer to the matching character or NULL
+ * Return: pointer to the matching byte, or NULL
  */
 char *_strpbrk(char *s, char *accept)
 {
- int i, j;
+	int i, j;
 
- for (i = 0; s[i]; i++)
- {
-  for (j = 0; accept[j]; j++)
-  {
-   if (s[i] == accept[j])
-    return (s + i);
-  }
- }
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; accept[j] != '\0'; j++)
+		{
+			if (s[i] == accept[j])
+				return (s + i);
+		}
+	}
 
+	return (0);
+}
  return (0);
 }
