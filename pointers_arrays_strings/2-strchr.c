9a940144982bcade054e7cfaa6cbacd895ae4535
strewili <shouqaaa123@gmail.com>
2026-03-09 21:05:16 +0300

    Update 2-strchr.c

--- pointers_arrays_strings/2-strchr.c
+++ pointers_arrays_strings/2-strchr.c
@@ -2,23 +2,23 @@
 
 /**
  * _strchr - locates a character in a string
- * @s: string
- * @c: character
+ * @s: string to search
+ * @c: character to find
  *
- * Return: pointer to character or NULL
+ * Return: pointer to first occurrence of c, or NULL
  */
 char *_strchr(char *s, char c)
 {
- int i;
+	int i;
 
- for (i = 0; s[i]; i++)
- {
-  if (s[i] == c)
-   return (s + i);
- }
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (s[i] == c)
+			return (s + i);
+	}
 
- if (s[i] == c)
-  return (s + i);
+	if (s[i] == c)
+		return (s + i);
 
- return (0);
+	return (0);
 }
