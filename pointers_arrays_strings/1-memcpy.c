d0fbc87c4a52f475516ff3adad488deb75990cf3
strewili <shouqaaa123@gmail.com>
2026-03-09 21:03:34 +0300

    Update 1-memcpy.c

--- pointers_arrays_strings/1-memcpy.c
+++ pointers_arrays_strings/1-memcpy.c
@@ -2,18 +2,18 @@
 
 /**
  * _memcpy - copies memory area
- * @dest: destination
- * @src: source
- * @n: number of bytes
+ * @dest: destination memory area
+ * @src: source memory area
+ * @n: number of bytes to copy
  *
- * Return: dest
+ * Return: pointer to dest
  */
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
- unsigned int i;
+	unsigned int i;
 
- for (i = 0; i < n; i++)
-  dest[i] = src[i];
+	for (i = 0; i < n; i++)
+		dest[i] = src[i];
 
- return (dest);
+	return (dest);
 }
